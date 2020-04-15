d662a23fcf6a2c6b4fc8be683e0cb8ab6191abb5
thanhdat2721 <nguyenthanhdat2721@gmail.com>
2020-04-14 23:05:09 -0500

    add modal when click report button

--- js/main.js
+++ js/main.js
@@ -44,20 +44,22 @@ window.onload = (function() {
     function onSuccess(response) {
         let circle;
         response.forEach((location) => {
-
             circle = L.circle([location.lat, location.lon], {
                 color: 'red',
                 fillColor: '#f03',
                 fillOpacity: 0.5,
                 radius: location.latestTotalCases
             }).addTo(map);
-            const popups = L.popup()
-                .setContent(`<b>Latest Total Cases</b>: ${location.latestTotalCases} <br /> 
-                             <b>Previous Day Difference</b>: ${location.diffFromPrevDay} <br />
-                             <b>Country</b>: ${location.country}
-                             <button type="button" class="forButton">Report</button>
-                            `);
+
+            const popups = L.DomUtil.create('div', 'infoWindow');
+            popups.innerHTML = `<b>Latest Total Cases</b>: ${location.latestTotalCases} <br /> 
+            <b>Previous Day Difference</b>: ${location.diffFromPrevDay} <br />
+            <b>Country</b>: ${location.country}
+            <button id="reportBtn" type="button" class="forButton">Report</button>`;
             circle.bindPopup(popups);
+            $('#reportBtn', popups).on('click', function(event) {
+                $("#myModal").css({ "display": "block" });
+            });
         });
         $(".loader").hide();
     }
@@ -67,46 +69,13 @@ window.onload = (function() {
         alert("ERROR OCCUR");
     }
 
-
-    $('#myBtn').on('click', function(event) {
-        $("#myModal").css({ "display": "block" });
-    });
-
     $('.custome-modal-header-close').on('click', function(event) {
         $("#myModal").css({ "display": "none" });
     });
-    console.log($("#myModal"));
 
     $(window).click(function(e) {
         if (e.target == $("#myModal")[0]) {
             $("#myModal").css({ "display": "none" });
         }
     });
-
-    // function processingData(data) {
-    //     const chinaState = data.filter(s => s.country === "China");
-    //     chinaState[0].state = [];
-    //     chinaState[0].latestTotalCases = 0;
-    //     chinaState[0].diffFromPrevDay = 0;
-    //     const china = Object.assign({}, chinaState[0]);
-    //     chinaState.shift();
-    //     chinaState.forEach(item => {
-    //         china.state.push(item);
-    //         china.latestTotalCases += item.latestTotalCases;
-    //         china.diffFromPrevDay += item.diffFromPrevDay;
-    //     });
-    //     const findCountry = data.filter(item => {
-    //         if (item.state.length === 0) {
-    //             const findState = data.filter(s => s.state.length !== 0 && s.country === item.country);
-    //             item.state = findState;
-    //             return item;
-    //         }
-    //     });
-    //     findCountry.push(china);
-    //     return findCountry;
-    // }
-
-    // const nest = (items, id = null, link = 'country') => items
-    //     .filter(item => item[link] === id)
-    //     .map(item => ({...item, diffFromPrevDay: diffFromPrevDay + item.diffFromPrevDay, latestTotalCases: latestTotalCases + item.latestTotalCases, state: nest(items, item.id) }));
 })();
