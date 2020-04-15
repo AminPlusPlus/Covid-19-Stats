7f28aa1afbe0b0d1310ca6e19617ce3c288f0bcc
Nis <36338262+bouncerate@users.noreply.github.com>
2020-04-15 16:01:10 -0500

    There were error in fetching.
    This should fix everything.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,16 @@
 /*global window */
 window.onload = (function() {
     "use strict";
-
     var map = L.map('mapid', {
         zoomControl: false,
         worldCopyJump: true
-    }).setView([0,0], 4);
+    }).setView([0,0], 1);
     const attribution = '&copy; <a href="https://www.openstreetmaps.org/copyright">OpenStreetMap</a> contributors';
     L.tileLayer(
         'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution,
             maxZoom: 18,
-            minZoom: 4,
+            minZoom: 2,
             tap: false,
             zoomControl: false,
         }).addTo(map);
@@ -21,54 +20,42 @@ window.onload = (function() {
         position: 'bottomright'
     }).addTo(map);
 
-    $('.btnSearch').on('click', function(event) {
-        event.preventDefault();
-        if (!openSearch) {
-            openSearch = true;
-            $('.slideout_inner').addClass('active');
-        } else {
-            openSearch = false;
-            $('.slideout_inner').removeClass("active");
-        }
-    });
+    $.ajax({
+        url: "https://covid-19-273501.appspot.com/api/v1/confirms",
+        type: "GET",
+        dataType: 'json',
+        contentType: 'application/json',
+        beforeSend: function() {
+            $(".loader").show();
+        },
+    }).done(onSuccess).fail(onFail);
 
-    (function() {
-        fetchData();
-    })();
+    function onSuccess(response) {
+        let circle;
+        response.forEach((location) => {
+            circle = L.circle([location.lat, location.lon], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: location.latestTotalCases
+            }).addTo(map);
 
-
-    function fetchData() {
-        //$(".loader").show();
-        fetch("https://covid-19-273501.appspot.com/api/v1/confirms", {
-                method: 'GET',
-            }).then(response => {
-                if (response.ok)
-                    return response.json();
-                else
-                    return Promise.reject({ status: response.status, statusText: response.statusText });
-            })
-            //After receiving the data,
-            .then(data => {
-                data.forEach((location) => {
-                    let circle = L.circle([location.lat, location.lon], {
-                        color: 'red',
-                        fillColor: '#f03',
-                        fillOpacity: 0.5,
-                        radius: location.latestTotalCases
-                    }).addTo(map);
-
-                    const popups = L.DomUtil.create('div', 'infoWindow');
-                    popups.innerHTML = `<b>Latest Total Cases</b>: ${location.latestTotalCases} <br /> 
-                     <b>Previous Day Difference</b>: ${location.diffFromPrevDay} <br />
-                     <b>Country</b>: ${location.country}
-                     <button id="reportBtn" type="button" class="forButton">Report</button>`;
-                    circle.bindPopup(popups);
-                    $('#reportBtn', popups).on('click', function(event) {
-                        $("#myModal").css({ "display": "block" });
-                    });
-                });
-                //$(".loader").hide();
+            const popups = L.DomUtil.create('div', 'infoWindow');
+            popups.innerHTML = `<b>Latest Total Cases</b>: ${location.latestTotalCases} <br /> 
+            <b>Previous Day Difference</b>: ${location.diffFromPrevDay} <br />
+            <b>Country</b>: ${location.country}
+            <button id="reportBtn" type="button" class="forButton">Report</button>`;
+            circle.bindPopup(popups);
+            $('#reportBtn', popups).on('click', function(event) {
+                $("#myModal").css({ "display": "block" });
             });
+        });
+        $(".loader").hide();
+    }
+
+    function onFail() {
+        $(".loader").hide();
+        alert("ERROR OCCURED");
     }
 
     $('.custome-modal-header-close').on('click', function(event) {
